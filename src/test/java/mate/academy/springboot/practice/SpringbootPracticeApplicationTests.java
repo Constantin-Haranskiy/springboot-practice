9bf02270353f359ea1531cbc11534104e886ffd6
Con <con@MacBook-Air-Con.lan>
2026-06-20 19:15:34 +0200

    Initial commit

---
+++ src/test/java/mate/academy/springboot/practice/SpringbootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.springboot.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
