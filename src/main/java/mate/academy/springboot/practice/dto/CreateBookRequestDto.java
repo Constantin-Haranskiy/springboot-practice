48850239b7c0b57116ee9bbe454c0587baf105c1
Con <con@MacBook-Air-Con.local>
2026-06-25 10:13:07 +0200

    Fixed validation in CreateBookRequestDto

--- src/main/java/mate/academy/springboot/practice/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/springboot/practice/dto/CreateBookRequestDto.java
@@ -1,22 +1,23 @@
 package mate.academy.springboot.practice.dto;
 
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
 import mate.academy.springboot.practice.validation.Isbn;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
-    @NotNull
+    @NotBlank
     @Isbn
     private String isbn;
     @NotNull
-    @Min(0)
+    @Positive
     private BigDecimal price;
     private String description;
     private String coverImage;
